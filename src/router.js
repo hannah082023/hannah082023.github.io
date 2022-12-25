693cb6cab68d8f8e522b878b0bfb52b60dcdff42
Hannah Su <hannah082023@gmail.com>
2022-12-25 14:25:53 +0800

    cancel redirect

--- src/router.js
+++ src/router.js
@@ -6,7 +6,6 @@ const routes = [
     { path: "/", name: "home", component: () => import("./views/Home.vue") },
     { path: "/about", name: "about", component: () => import("./views/About.vue") },
     { path: "/projects", name: "projects", component: () => import("./views/Projects.vue") },
-    { path: "/:catchAll(.*)*", redirect: "/" },
 ];
 
 export const router = createRouter({
