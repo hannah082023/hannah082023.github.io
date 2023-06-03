0032dc1501f59509bb37484f445041dd5446da32
Hannah Su <hannah082023@gmail.com>
2023-06-03 18:33:24 +0800

    redirect

--- src/router.js
+++ src/router.js
@@ -6,6 +6,7 @@ const routes = [
     { path: "/", name: "home", component: () => import("./views/Home.vue") },
     { path: "/about", name: "about", component: () => import("./views/About.vue") },
     { path: "/projects", name: "projects", component: () => import("./views/Projects.vue") },
+    { path: "/:catchAll(.*)*", redirect: "/" },
 ];
 
 export const router = createRouter({
