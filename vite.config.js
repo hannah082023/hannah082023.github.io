b4c7c61fcd17da6853950efe6a6c11773262b3db
Hannah Su <hannah082023@gmail.com>
2026-05-19 03:44:36 +0800

    Service worker allow "SimpleWeb"

--- vite.config.js
+++ vite.config.js
@@ -25,6 +25,7 @@ export default defineConfig({
                     },
                 ],
                 skipWaiting: true,
+                navigateFallbackDenylist: [/^\/SimpleWeb/]
             },
         }),
     ],
